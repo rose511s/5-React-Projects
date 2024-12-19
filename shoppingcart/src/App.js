6325bed705ca899cc4471da2c199e004382d901b
rose511s <kratikasingh397@gmail.com>
2024-12-19 14:04:09 +0530

    first commit

---
+++ shoppingcart/src/App.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import Navbar from './components/Navbar'
+import Shop from './components/Shop'
+import Cart from './components/Cart'
+import './App.css'
+
+const App = () => {
+
+  const [cart, setCart] = useState([]);
+  const [warning, setWarning] = useState(false);
+  const [show, setShow] = useState(true);
+
+  const handleClick=(item) => {
+    let isPresent = false;
+    cart.forEach((product) => {
+      if (item.id === product.id) {
+        isPresent = true;
+      }
+    })
+    if (isPresent) {
+      setWarning(true);
+      setTimeout(() => {
+        setWarning(false);
+      }, 2000);
+      return;
+    }                
+    setCart([...cart, item]);
+  }
+
+  const handleChange = (item, d) => {
+    let ind = -1;
+    cart.forEach((data,index)=> {
+        if (data.id === item.id) {
+          ind = index;
+      }
+})
+const tempArr = cart;
+tempArr[ind].amount += d; 
+
+    if (tempArr[ind].amount === 0) {
+      tempArr[ind].amount = 1;
+      
+    }
+    setCart([...tempArr]); 
+  }
+  return (
+    <div>
+      <Navbar size={cart.length} setShow={setShow} />
+      {
+        show ? <Shop handleClick={handleClick} /> : <Cart cart={cart} setCart={setCart} handleChange={handleChange}/>
+      }
+      <Shop handleClick={handleClick} />
+      {
+        warning && <div className='warning'>Item is already in your cart </div>
+      }
+    </div>
+  )
+}
+
+export default App
